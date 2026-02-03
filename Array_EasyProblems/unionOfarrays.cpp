51834ef8812abc5719418e438fcf193b59a6e4b7
Ganesh Yandigeri <ganiy0789@gmail.com>
2026-02-03 21:51:23 +0530

    added

--- Array_EasyProblems/unionOfarrays.cpp
+++ Array_EasyProblems/unionOfarrays.cpp
@@ -1,53 +1,41 @@
-#include <iostream>
-#include <vector>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-vector<int> findUnionSorted(const vector<int>& a, const vector<int>& b) {
-    int n = a.size();
-    int m = b.size();
+vector<int> sortedArray(vector<int> &a, vector<int> &b) {
     int i = 0, j = 0;
-    vector<int> unionRes;
+    int n1 = a.size(), n2 = b.size();
+    vector<int> unionArr;
 
-    while (i < n && j < m) {
-        // Pick the smaller element to maintain sorted order
-        if (a[i] <= b[j]) {
-            // Only add if it's the first element or not a duplicate of the last added
-            if (unionRes.empty() || unionRes.back() != a[i]) {
-                unionRes.push_back(a[i]);
-            }
-            // Move both pointers if elements are equal to skip redundant checks
-            if (a[i] == b[j]) j++;
+    while (i < n1 && j < n2) {
+        if (a[i] == b[j]) {
+            if (unionArr.empty() || unionArr.back() != a[i])
+                unionArr.push_back(a[i]);
             i++;
-        } else {
-            if (unionRes.empty() || unionRes.back() != b[j]) {
-                unionRes.push_back(b[j]);
-            }
+            j++;
+        }
+        else if (a[i] < b[j]) {
+            if (unionArr.empty() || unionArr.back() != a[i])
+                unionArr.push_back(a[i]);
+            i++;
+        }
+        else {
+            if (unionArr.empty() || unionArr.back() != b[j])
+                unionArr.push_back(b[j]);
             j++;
         }
     }
 
-    // Add remaining elements from array 'a'
-    while (i < n) {
-        if (unionRes.back() != a[i]) unionRes.push_back(a[i]);
+    while (i < n1) {
+        if (unionArr.empty() || unionArr.back() != a[i])
+            unionArr.push_back(a[i]);
         i++;
     }
 
-    // Add remaining elements from array 'b'
-    while (j < m) {
-        if (unionRes.back() != b[j]) unionRes.push_back(b[j]);
+    while (j < n2) {
+        if (unionArr.empty() || unionArr.back() != b[j])
+            unionArr.push_back(b[j]);
         j++;
     }
 
-    return unionRes;
-}
-
-int main() {
-    vector<int> a = {1, 1, 2, 2, 3, 4};
-    vector<int> b = {2, 2, 3, 4, 5, 6};
-
-    vector<int> result = findUnionSorted(a, b);
-
-    for (int x : result) cout << x << " "; // Output: 1 2 3 4 5 6
-    return 0;
+    return unionArr;
 }
