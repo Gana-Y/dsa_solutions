4fbb47deee8a0f5777b2a214eaba4818a462f815
Ganesh Yandigeri <ganiy0789@gmail.com>
2026-02-03 22:53:08 +0530

    added

--- Array_EasyProblems/unionOfarrays.cpp
+++ Array_EasyProblems/unionOfarrays.cpp
@@ -39,3 +39,24 @@ vector<int> sortedArray(vector<int> &a, vector<int> &b) {
 
     return unionArr;
 }
+
+int main() {
+    int n1, n2;
+    cin >> n1;
+
+    vector<int> a(n1);
+    for (int i = 0; i < n1; i++)
+        cin >> a[i];
+
+    cin >> n2;
+    vector<int> b(n2);
+    for (int i = 0; i < n2; i++)
+        cin >> b[i];
+
+    vector<int> result = sortedArray(a, b);
+
+    for (int x : result)
+        cout << x << " ";
+
+    return 0;
+}
