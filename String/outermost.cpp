83dea90e14618b34080a9b1612dd807f1c576f0a
Ganesh Yandigeri <ganiy0789@gmail.com>
2026-05-02 18:36:04 +0530

    added string

---
+++ String/outermost.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+string RemoveoutermostString(string s)
+{
+   int  count=0;
+   string result;
+    for(char c:s)
+    {
+         if(c=='(')
+         {
+            if(count>0)
+              result+=c;
+            count++;
+         }
+         else{
+             count--;
+             if(count>0)
+             result+=c;
+         }
+    }
+    return result;
+}
+int main()
+{
+    string name;
+    cout << "Enter Parentheses:" << endl;
+    cin >> name;
+
+    string output=RemoveoutermostString(name);
+    cout<<output;
+}
