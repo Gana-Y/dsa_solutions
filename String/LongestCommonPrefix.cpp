a91bad1b97c8a3433dac0440623dcfecb04ab5ac
Ganesh Yandigeri <ganiy0789@gmail.com>
2026-05-09 18:56:49 +0530

    add

---
+++ String/LongestCommonPrefix.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string LongestCommonPrefix(vector<string>& strs)
+{
+    string prefix="";
+    for(int i=0;i<strs[0].size();i++)
+    {
+        char c=strs[0][i];
+
+        for(int j=1;j<strs.size();j++)
+        {
+            if(i>=strs[j].size() || strs[j][i] !=c )
+            {
+               return prefix;
+            }
+        }
+        prefix += c;
+    }
+ 
+    return "";
+
+}
+int main()
+{
+     int n;
+      cout<<"Enter the size of  String"<<endl;
+     cin>>n;
+    
+    vector<string> names(n);
+     cout<<"Enter Names to find Longest Prefix"<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cin>>names[i];
+    }
+    cout <<"Longest Common Prefix is:"<< LongestCommonPrefix(names)<<endl;
+}
